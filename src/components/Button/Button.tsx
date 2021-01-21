39afafe127e9a114ecc99038170194b185bf92f8
ILya Schetchikov <IESchetchikov@sberbank.ru>
2021-01-21 10:58:34 +0300

    init3

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,25 +1,5 @@
 import React from 'react';
-
-const style = {
-  ".storybook-button": {
-    fontFamily: "'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-    fontWeight: 700,
-    border: "0",
-    borderRadius: "3em",
-    cursor: "pointer",
-    display: "inline-block",
-    lineHeight: 1
-  },
-  ".storybook-button--primary": { color: "white", backgroundColor: "#1ea7fd" },
-  ".storybook-button--secondary": {
-    color: "#333",
-    backgroundColor: "transparent",
-    boxShadow: "rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset"
-  },
-  ".storybook-button--small": { fontSize: "12px", padding: "10px 16px" },
-  ".storybook-button--medium": { fontSize: "14px", padding: "11px 20px" },
-  ".storybook-button--large": { fontSize: "16px", padding: "12px 24px" }
-}
+import './button.css';
 
 export interface ButtonProps {
   /**
